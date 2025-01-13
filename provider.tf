262cb815e144e4020d75ed9a3688c0a81c7add47
wuyvzhang <wuyvzhang@hotmail.com>
2025-01-13 14:43:27 +0800

    first commit

--- provider.tf
+++ provider.tf
@@ -17,7 +17,5 @@ terraform {
       storage_account_name = "tfstate0113"
       container_name       = "tfstate"
       key                  = "tfstate"
-      tenant_id            = "a55b816c-e99d-4af2-b69c-60fb4f03bef5"
-      subscription_id      = "177add63-2747-4d6d-a5e4-004dc63b04c6"
     }
 }
